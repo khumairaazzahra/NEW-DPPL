713ebad349d3c58161d6888c8b5ad1d589f78512
khumairaazzahra <khumairayarman2004@gmail.com>
2025-11-03 09:55:53 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nama  : Paul");
+        System.out.println("NIM   : 1234 ");
+
+        System.out.println("Nama  : Maira");
+        System.out.println("NIM   : 2345");
+    }
+}
