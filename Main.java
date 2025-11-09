c8fa2e6b25b86853e98d82bf7cca5b41ea13f8ad
rifaulkhairunisa0731 <rifaul.khairunisa0731@student.unri.ac.id>
2025-11-09 12:15:46 +0700

    delete main

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Nama  : Paul");
-        System.out.println("NIM   : 1234 ");
-
-        System.out.println("Nama  : Maira");
-        System.out.println("NIM   : 2345");
-    }
-}
